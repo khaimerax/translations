3e5822f628ce50fd90d11638de1b1ccea225474f
khaimerax <khaimerax@gmail.com>
2025-04-28 11:56:15 +0300

    Update

---
+++ fr/vendor/plugins/ecommerce/fr/brands.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'name' => 'Brands',
+    'create' => 'New brand',
+    'edit' => 'Edit brand',
+    'form' => [
+        'description' => 'Description',
+        'logo' => 'Logo',
+    ],
+    'logo' => 'Logo',
+    'website' => 'Website',
+    'notices' => [
+        'no_select' => 'Please select at least one record to take this action!',
+        'update_success_message' => 'Update successfully',
+    ],
+    'cannot_delete' => 'Brand could not be deleted',
+    'brand_deleted' => 'Brand deleted',
+    'menu' => 'Brands',
+    'select_brand' => 'Sélectionnez une marque...',
+    'intro' => [
+        'title' => 'Manage brands',
+        'description' => 'Manage product brands such as Nike, Adidas, Bitis ...',
+        'button_text' => 'Create brand',
+    ],
+    'edit_this_brand' => 'Modifier cette marque',
+];
---
+++ fr/vendor/plugins/ecommerce/fr/product-label.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'name' => 'Product labels',
+    'create' => 'New product label',
+    'edit' => 'Edit product label',
+    'color' => 'Color',
+    'color_placeholder' => 'Exemple : #f0f0f0',
+];
